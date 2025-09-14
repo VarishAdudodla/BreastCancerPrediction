6c07487f9db43f5330f8d4218c3efc1bfe4b290e
Varish Adudodla <varish.adu@gmail.com>
2025-09-14 18:11:54 -0500

    Created dataprep.py to support multiple Models

--- breastcancer.py
+++ breastcancer.py
@@ -5,27 +5,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.preprocessing import StandardScaler
+from dataprep import load_data
 
-df = pd.read_csv('breast-cancer.csv')
-df = df.drop(columns=['id'])
-df['diagnosis'] = df['diagnosis'].map({'M': 1, 'B': 0})
-
-corr = df.corr()
-first_row = corr.iloc[0]
-sorted_row = first_row.abs().sort_values(ascending=False)
-
-## Select top 5 features correlated with diagnosis: concave points_worst, perimeter_worst, concave points_mean, radius_worst, perimeter_mean
-features = ['concave points_worst', 'perimeter_worst', 'concave points_mean', 'radius_worst', 'perimeter_mean']
-X = df[features]
-y = df['diagnosis']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Standardize Features
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
-
-#Create, Train and Evaluate Model
+X_train, X_test, y_train, y_test = load_data()
 
 model = LogisticRegression()
 model.fit(X_train, y_train)
